50d0f6e70510c36e84a5cbd06446229a66f9d4ce
Travis Warlick <twarlick@gmail.com>
2015-03-02 14:20:01 -0500

    Add update-public script to download & extract public files

--- script/NetworkManager-dispatcher.d/90-update-sfv2.sh
+++ script/NetworkManager-dispatcher.d/90-update-sfv2.sh
@@ -10,5 +10,9 @@ echo "[$DTS] $if_name $status" >> $LOGFILE
 
 if [[ $status == "up" ]]; then
   if [[ $if_name == "enp0s25" ]] || [[ $if_name == "wlp2s0" ]]; then
+    pwd_orig=`pwd`
+    cd /srv/sfv2/current/
+    ./script/update-public.sh
+    cd $pwd_orig
   fi
 fi
