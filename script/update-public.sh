c867f5930eb977350e75537c2190f7d93eca2b65
Travis Warlick <twarlick@gmail.com>
2015-03-02 15:12:34 -0500

    Change package domain to use dev and add uploads/ to path

--- script/update-public.sh
+++ script/update-public.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 SHOWROOM_MD5=` cat showroom.tar.gz.md5 `
-showroom_md5=` curl -sS https://staging.sfv2.cox.mxmcloud.com/showroom.tar.gz.md5 `
+showroom_md5=` curl -sS https://dev.sfv2.cox.mxmcloud.com/uploads/showroom.tar.gz.md5 `
 
 if [[ $SHOWROOM_MD5 != $showroom_md5 ]]; then
-  curl -sS -O https://staging.sfv2.cox.mxmcloud.com/showroom.tar.gz
+  curl -sS -O https://dev.sfv2.cox.mxmcloud.com/uploads/showroom.tar.gz
   tar -xzf showroom.tar.gz
   rm showroom.tar.gz
   echo $showroom_md5 > showroom.tar.gz.md5
