d879f809b51121dd03ed92643b10d05c25f0f0dd
Travis Warlick <twarlick@gmail.com>
2015-03-06 17:54:26 -0500

    modularize app & mount at /stats/

--- index.js
+++ index.js
@@ -5,212 +5,64 @@ require('dotenv').load();
 var async = require('async')
   , express = require('express')
   , bodyParser = require('body-parser')
-  , app = express()
-  , server = require('http').Server(app)
-  , io = require('socket.io').listen(server)
+  , http = require('http')
+  , socketIO = require('socket.io')
   , fs = require('fs')
   , mkdirp = require('mkdirp')
   , _ = require('lodash')
-  , dataDir = 'data/'
-  , archiveDir = 'archive/'
-  , settingsDir = 'settings/'
-  , settingsFile = settingsDir + 'settings.json';
+  , recordsApp = require('./lib/records')
+  , settingsApp = require('./lib/settings');
 
-
-var port = process.env.PORT;
+var port = process.env.PORT,
+    app = express(),
+    server = http.Server(app),
+    io = socketIO.listen(server);
 
 // for parsing application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/_status_/heartbeat', function (req, res) {
   res.type("text").send("OK");
 });
 
-// get settings json
-app.get('/settings.json', function(req, res) {
-  fs.readFile(settingsFile, {encoding: 'utf8'}, function(err, content){
-    if (err) {
-      res.json({ status: 'error', message: err })
-    } else {
-      res.json({ status: 'success', data: content })
-    }
-  })
-})
+var statsRoute = express.Router();
+recordsApp(statsRoute);
+settingsApp(statsRoute);
+app.use('/stats', statsRoute);
 
-// write settings json
-app.post('/settings.json', function(req, res) {
-  var json = req.body
+// Listen for TV-Tablet Transfer
+io.of('/socket.io').on('connection', function(socket) {
+  console.log('Socket connection established');
 
-  fs.writeFile(settingsFile, JSON.stringify(json), function(err){
-    if (err) {
-      res.json({ status: 'error', message: err })
-    } else {
-      res.json({ status: 'success', data: json })
-    }
-  })
-})
-
-// record json
-app.post('/record.json', function(req, res) {
-  var json = req.body
-    , name = (new Date).getTime()
-
-  // Add id to test for duplicates during processing
-  json._id = name;
-
-  fs.writeFile(dataDir + name + '.json', JSON.stringify(json), function(err){
-    if (err) {
-      res.json({ status: 'error', message: err })
-    } else {
-      res.json({ status: 'success', data: json })
-    }
-  })
-})
-
-// app.get('/socket.io/', function(req, res) {
-//   console.log('forwarded');
-// });
-
-app.get('/records.json', function(req, res) {
-  var data = []
-
-	// First try to read the settings file
-  fs.readFile(settingsFile, {encoding: 'utf8'}, function(err, content){
-    if (! err) {
-      data.push(content)
-    }
-
-		// Now read all the great data
-		fs.readdir(dataDir, function(err, files){
-			if (err) {
-				return res.json({ status: 'error', message: err })
-			}
-
-
-			function readFile(ndx, callback) {
-				if (ndx < files.length){
-					fs.readFile(dataDir + files[ndx], {encoding: 'utf8'}, function(err, content){
-						if (err) {
-							callback(err)
-						} else {
-							data.push(content)
-							readFile(ndx + 1, callback)
-						}
-					})
-				} else {
-					callback()
-				}
-			}
-
-			function moveFile(ndx, callback) {
-				if (ndx < files.length) {
-					fs.rename(dataDir + files[ndx], archiveDir + files[ndx], function(err){
-							if (err) {
-								callback(err)
-							} else {
-								moveFile(ndx + 1, callback)
-							}
-					})
-				} else {
-					callback()
-				}
-			}
-
-			async.waterfall([
-				function(callback){
-					readFile(0, callback)
-				},
-				function(callback){
-					moveFile(0, callback)
-				},
-			],function(err){
-					if (err) {
-						res.json({ status: 'error', message: err })
-					}else{
-						res.json({ status: 'success', data: data })
-					}
-			})
-
-		})
-  })
-})
-
-//Listen for TV-Tablet Transfer
-io.on('connection', function(socket){
-  console.log('it is working?');
-  socket.on('request', function(msg){
-    console.log('heard you');
+  socket.on('request', function(msg) {
     io.emit('request', msg);
   });
 
-  socket.on('response', function(msg){
-    console.log('heard you');
+  socket.on('response', function(msg) {
     io.emit('response', msg);
-  });  
+  });
 });
 
-// app.get('/touch', function(req, res){
-//     res.sendFile(__dirname + '/touch.html');
-// });
-
-// app.get('/tablet', function(req, res){
-//     res.sendFile(__dirname + '/tablet.html');
-// });
-
+if(fs.existsSync(__dirname + '/public')) {
+  app.use(express.static(__dirname + '/public'));
+}
 
 // Ensure port is set
 function ensurePort(callback) {
-  console.log("Ensuring port")
-  if (! port){
-    callback(new Error("Port not set"))
+  if (!port) {
+    callback(new Error("Port not set"));
   }
-
-  callback()
-}
-
-// Ensure dataDir exists
-function ensureDataDir(callback) {
-  console.log("Ensuring data/")
-  mkdirp(dataDir, function(err){
-    if (err){
-      callback(err)
-    }
-    callback()
-  })
-}
-
-// Ensure archiveDir exists
-function ensureArchiveDir(callback) {
-  console.log("Ensuring archive/")
-  mkdirp(archiveDir, function(err){
-    if (err){
-      callback(err)
-    }
-    callback()
-  })
-}
-
-// Ensure settingsDir exists
-function ensureSettingsDir(callback) {
-  console.log("Ensuring settings/")
-  mkdirp(settingsDir, function(err){
-    if (err){
-      callback(err)
-    }
-    callback()
-  })
+  callback();
 }
 
 async.waterfall([
   ensurePort,
-  ensureDataDir,
-  ensureArchiveDir,
-  ensureSettingsDir
-],function(err){
-  if (err){
-    console.log(err)
+], function(err) {
+  if (err) {
+    console.error(err);
     exit(1)
   }
-  server.listen(port, function(){ console.log('Server listening now on : ' + port); } );
-})
-
+  server.listen(port, function() {
+    console.log('Server listening now on : ' + port);
+  });
+});
